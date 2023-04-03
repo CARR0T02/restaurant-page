8d266265b24b4117c3e15b3ff0cb38c558d875e2
CARR0T02 <mr.gabriel.chang@gmail.com>
2023-04-03 08:45:06 +0800

    Added elementObj for homepage

--- src/home.js
+++ src/home.js
@@ -1,4 +1,12 @@
-function loadHome() {
-  const container = document.querySelector('#content');
-  let header = document.createElement('');
-}
+import * as content from './content';
+
+export const homeContent = [
+  content.createElementObj('div', '', 'flex flex-col', [
+    content.createElementObj('h1', "Gabe's Italian"),
+    content.createElementObj(
+      'h2',
+      'Nothing brings people together like good food.'
+    ),
+    content.createElementObj('button', 'Menu'),
+  ]),
+];
